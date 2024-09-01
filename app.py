51b795748480728e1d300401306f378497e92cda
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-01 21:18:52 +0500

    add user handler to app file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models.database_orm import engine, Base
-from handlers import posts_handler
+from handlers import posts_handler, users_handler
 
 
 Base.metadata.create_all(bind=engine)
@@ -14,4 +14,4 @@ def home():
 
 
 app.include_router(posts_handler.router)
-
+app.include_router(users_handler.router)
