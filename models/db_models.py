0b01cf1a90561c10251471f97c9cf5f92c8df8b3
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-01 21:23:02 +0500

    Fix issue. [related to: default set in database]

--- models/db_models.py
+++ models/db_models.py
@@ -7,9 +7,9 @@ class Users(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String, nullable=False)
     user_name = Column(String, nullable=False, unique=True)
-    email = Column(String, default=0, unique=True)
-    password = Column(String, default=0)
-    created_at = Column(DateTime, default=func.now())
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
 
 
 class Posts(Base):
@@ -18,8 +18,8 @@ class Posts(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    likes = Column(Integer, default=0)
-    created_at = Column(DateTime, default=func.now())
+    likes = Column(Integer, server_default="0")
+    created_at = Column(DateTime, server_default=func.now())
 
 
 class Profiles(Base):
@@ -27,8 +27,8 @@ class Profiles(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     bio = Column(String, nullable=False)
-    profile_pic = Column(String, default="None")
-    created_at = Column(DateTime, default=func.now())
+    profile_pic = Column(String, server_default="None")
+    created_at = Column(DateTime, server_default=func.now())
 
 class Comments(Base):
     __tablename__ = "comments"
@@ -36,11 +36,11 @@ class Comments(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'))
     content = Column(String, nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, server_default=func.now())
 
 class Follows(Base):
     __tablename__ = "follows"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     follower_id = Column(Integer, ForeignKey('users.id'))
     following_id = Column(Integer, ForeignKey('users.id'))
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, server_default=func.now())
