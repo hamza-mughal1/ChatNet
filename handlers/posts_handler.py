47d73c29369f7c65a6fb1a086e4957ff9657ed99
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-02 21:22:34 +0500

    Add endpoint 'create_post' to create a post

--- handlers/posts_handler.py
+++ handlers/posts_handler.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter, Depends
-from models.database_orm import get_db
-from sqlalchemy.orm import Session
+from models import db_models
 from models.posts_model import PostsModel
 from models import schemas
 from typing import List
 from Oauth2 import verify_token
+import utils
+
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 posts_model = PostsModel()
 
 @router.get("/", response_model=List[schemas.PostOut])
-def all_posts(db : Session =  Depends(get_db), token_data: dict = Depends(verify_token)):
+def all_posts(db : utils.db_dependency, token_data: dict = Depends(verify_token)):
     return posts_model.get_all_posts(db, token_data)
+
+@router.post("/", response_model=schemas.PostOut)
+def create_post(db: utils.db_dependency, post_data: schemas.CreatePost, token_data: dict = Depends(verify_token)):
+    return posts_model.create_post(db, post_data, token_data)
+
+@router.get("/{id}", response_model=schemas.PostOut)
+def get_post(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
+    return posts_model.get_post(db, id)
--- models/posts_model.py
+++ models/posts_model.py
@@ -1,6 +1,7 @@
 import utils
 from models.db_models import Posts as DbPostModel
-
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 class PostsModel():
     def __init__(self):
@@ -8,3 +9,18 @@ class PostsModel():
     
     def get_all_posts(self, db : utils.db_dependency, token_data):
         return db.query(DbPostModel).all()
+    
+    def create_post(self, db, post_data, token_data):
+        post = DbPostModel(user_id = token_data["user_id"], **post_data.model_dump())
+        db.add(post)
+        db.commit()
+
+        return post
+        
+    def get_post(self, db: Session, id):
+        post = db.query(DbPostModel).filter(DbPostModel.id == id).first()
+
+        if not post:
+            raise HTTPException(status_code=404, detail="No post found")
+        
+        return post
