212a5630317d8ff6c633b4eded88e3d670a1751c
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-02 10:35:40 +0500

    Add password validation using pydantic
    Password validation using pydantic 2.0 'filed_validator'

--- models/schemas.py
+++ models/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator, FieldValidationInfo
 from datetime import datetime
+import re
 
 class PostOut(BaseModel):
     id: int
@@ -26,3 +27,23 @@ class CreateUser(BaseModel):
     user_name: str
     email: EmailStr
     password: str
+
+    @field_validator('password')
+    def validate_password(cls, value: str, info: FieldValidationInfo):
+        # Check for length between 8 and 10 characters
+        if not 8 <= len(value) <= 10:
+            raise ValueError('Password must be between 8 and 10 characters.')
+
+        # Check for at least one uppercase letter
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('Password must contain at least one uppercase letter.')
+
+        # Check for at least one digit
+        if not re.search(r'\d', value):
+            raise ValueError('Password must contain at least one digit.')
+
+        # Check for at least one special character
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError('Password must contain at least one special character.')
+
+        return value
