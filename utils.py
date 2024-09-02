d0025c5da49fd5e26313ed4ea93e774e977e460e
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-02 16:09:37 +0500

    Add db_dependency util

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from passlib.context import CryptContext
-
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from models.database_orm import get_db
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_hashed_password(passowrd):
     return pwd_context.hash(passowrd)
+
+def verify_password(password, hashed_password):
+    return pwd_context.verify(password, hashed_password)
+
+db_dependency = Annotated[Session, Depends(get_db)]
