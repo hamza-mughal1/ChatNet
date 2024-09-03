f880c042d5dccd733ab40cbf33ed90b90e38cbbc
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-03 20:43:10 +0500

    Add comments route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models.database_orm import engine, Base
-from handlers import posts_handler, users_handler
+from handlers import posts_handler, users_handler, comments_handler
 from models.auth_model import router as auth_router
 
 Base.metadata.create_all(bind=engine)
@@ -15,4 +15,5 @@ def home():
 
 app.include_router(posts_handler.router)
 app.include_router(users_handler.router)
+app.include_router(comments_handler.router)
 app.include_router(auth_router)
