383a3430a75bab2f64a6d97a1a707b81441bd133
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-03 13:01:24 +0500

    Add orm to dict function

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from models.database_orm import get_db
+from sqlalchemy.inspection import inspect
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,3 +14,6 @@ def verify_password(password, hashed_password):
     return pwd_context.verify(password, hashed_password)
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+def orm_to_dict(obj):
+    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
