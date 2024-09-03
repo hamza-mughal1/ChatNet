14e164603f2b9e3cdd68564e13130b0b8282d851
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-03 12:59:45 +0500

    Add endpoints ('get_user', 'update_user', 'delete_user'))

--- handlers/users_handler.py
+++ handlers/users_handler.py
@@ -1,18 +1,29 @@
 from fastapi import APIRouter, Depends
-from models.database_orm import get_db
-from sqlalchemy.orm import Session
 from models.users_model import UsersModel
 from models import schemas
 from typing import List
-
+from Oauth2 import verify_token
+from utils import db_dependency
 router = APIRouter(prefix="/users", tags=["users"])
 
 users_model = UsersModel()
 
 @router.get("/", response_model=List[schemas.UserOut])
-def all_users(db : Session =  Depends(get_db)):
+def all_users(db : db_dependency, token_data: dict = Depends(verify_token)):
     return users_model.get_all_users(db)
 
 @router.post("/", response_model=schemas.UserOut, status_code=201)
-def create_user(user: schemas.CreateUser, db : Session =  Depends(get_db)):
+def create_user(user: schemas.CreateUser, db:  db_dependency):
     return users_model.create_user(user, db)
+
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(db :  db_dependency, id: int, token_data: dict = Depends(verify_token)):
+    return users_model.get_user(db, id, token_data)
+
+@router.put("/", response_model=schemas.UserOut)
+def update_user(db :  db_dependency, user_data: schemas.UpdateUser, token_data: dict = Depends(verify_token)):
+    return users_model.update_user(db, user_data, token_data)
+
+@router.delete("/", response_model=schemas.UserOut)
+def delete_user(db :  db_dependency, token_data: dict = Depends(verify_token)):
+    return users_model.delete_user(db, token_data)
--- models/users_model.py
+++ models/users_model.py
@@ -1,13 +1,21 @@
-from models.db_models import Users as DbUserModel
+from models.db_models import Users as DbUserModel, Follows
 from fastapi import HTTPException
 import utils
+from models import schemas
+from sqlalchemy.exc import IntegrityError
 
 class UsersModel():
     def __init__(self):
         pass
 
-    def get_all_users(self, db : utils.db_dependency):
-        return db.query(DbUserModel).all()
+    @staticmethod
+    def dict_with_follow(db, user):
+        followers = db.query(Follows).filter(Follows.follower_id == user.id).count()
+        following = db.query(Follows).filter(Follows.following_id == user.id).count()
+        return {"followers":followers, "following":following,**utils.orm_to_dict(user)}
+
+    def get_all_users(self, db : utils.db_dependency):    
+        return [UsersModel.dict_with_follow(db, i) for i in (db.query(DbUserModel).all())] 
     
     def create_user(self, user_info, db: utils.db_dependency):
         if db.query(DbUserModel).filter(DbUserModel.user_name == user_info.user_name).first():
@@ -21,5 +29,39 @@ class UsersModel():
         
         db.add(user)
         db.commit()
-        return user
+        return UsersModel.dict_with_follow(db, user)
     
+    def get_user(self, db: utils.db_dependency, id, token_data):
+        if (user := db.query(DbUserModel).filter(DbUserModel.id == id).first()) is None:
+            raise HTTPException(status_code=404, detail="user not found")
+    
+        return UsersModel.dict_with_follow(db, user)
+    
+
+    def update_user(self, db: utils.db_dependency, user_data: schemas.UpdateUser, token_data):
+        user = db.query(DbUserModel).filter(DbUserModel.id == token_data["user_id"]).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        user.name = user_data.name
+        try:
+            user.user_name = user_data.user_name 
+            user.bio = user_data.bio
+            user.email = user_data.email
+            db.commit()
+        except IntegrityError as e:
+            if "psycopg2.errors.UniqueViolation" in e.args[0]:
+                error_filed = e.args[0].split("DETAIL:  Key (")[1].split(")=")[0]
+            raise HTTPException(status_code=409, detail=f"{error_filed} already exists")
+    
+        return UsersModel.dict_with_follow(db, user)
+    
+    def delete_user(self, db: utils.db_dependency, token_data):
+        user = db.query(DbUserModel).filter(DbUserModel.id == token_data["user_id"]).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        db.delete(user)
+        db.commit()
+
+        return UsersModel.dict_with_follow(db, user)
