4da20dbd20097a62cd00fa83675825fa0c89cfcc
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-04 12:06:16 +0500

    Refactor 'verify_token' func. Changes: [fetching up to date 'user_name' from the db]

--- Oauth2.py
+++ Oauth2.py
@@ -2,6 +2,8 @@ from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
+from utils import db_dependency
+from models.db_models import Users
 
 SECRET_KEY = "77cfbtdl757pu7n526qng21g4ib3?2yy8n9dvj3arn4x52j183jyjunlrxcds6r6"
 ALGORITHM = "HS256"
@@ -19,7 +21,7 @@ def create_access_token(data: dict):
 
     return ecoded_token
 
-def verify_token(token : str = Depends(oauth2_baerer)):
+def verify_token(db :  db_dependency, token : str = Depends(oauth2_baerer)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         user_id = payload.get("user_id")
@@ -27,6 +29,11 @@ def verify_token(token : str = Depends(oauth2_baerer)):
 
         if (user_id is None) or (user_name is None):
             raise HTTPException(status_code=403, detail="token is invalid", headers = {"WWW-Authenticate":"Bearer"})
+        
+        if (user := db.query(Users).filter(Users.id == user_id).first()) is None:
+            raise HTTPException(status_code=403, detail="Invalid token. (User not found)", headers = {"WWW-Authenticate":"Bearer"})
+        
+        payload.update({"user_name": user.user_name})
     
     except JWTError:
         raise HTTPException(status_code=403, detail="token is invalid", headers={"WWW-Authenticate":"Bearer"})
