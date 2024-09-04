497b546321688cb9ac07bd3e9a731157f37e2514
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-04 11:46:11 +0500

    Remove unnecessary authentication

--- handlers/comments_handler.py
+++ handlers/comments_handler.py
@@ -14,9 +14,10 @@ def comment_on_posts(db : utils.db_dependency, id: int, comment: schemas.CreateC
     return comments_model.comment_on_post(db, id, comment, token_data)
 
 @router.get("/{id}", response_model=List[schemas.CommentOut])
-def get_comment_by_post_id(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
-    return comments_model.get_comment_by_post_id(db, id, token_data)
+def get_comment_by_post_id(db : utils.db_dependency, id: int):
+    return comments_model.get_comment_by_post_id(db, id)
 
 @router.delete("/{id}", response_model=schemas.CommentOut)
 def delete_comment_by_comment_id(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
     return comments_model.delete_comment(db, id, token_data)
+
