664c25d8b55a3714369385a3c30784d4ac2e90a0
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-04 15:24:46 +0500

    Add 'ChangePassword' schema to validate input for 'change password'

--- models/schemas.py
+++ models/schemas.py
@@ -98,3 +98,28 @@ class FollowList(BaseModel):
     follower_user_name: str
     following_user_name: str 
     created_at: datetime
+
+class ChangePassword(BaseModel):
+    email: EmailStr
+    password: str
+    new_password: str
+
+    @field_validator('password', 'new_password')
+    def validate_password(cls, value: str, info: FieldValidationInfo):
+        # Check for length between 8 and 10 characters
+        if not 8 <= len(value) <= 10:
+            raise ValueError('Password must be between 8 and 10 characters.')
+
+        # Check for at least one uppercase letter
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('Password must contain at least one uppercase letter.')
+
+        # Check for at least one digit
+        if not re.search(r'\d', value):
+            raise ValueError('Password must contain at least one digit.')
+
+        # Check for at least one special character
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError('Password must contain at least one special character.')
+
+        return value
