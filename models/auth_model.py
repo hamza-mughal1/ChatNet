8de0c76f9f8fbab9ef516d465aa695da90ca1bcf
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-11 11:36:07 +0500

    Add 'logout-all' endpoint in 'auth_model'
    It allows users to logout from all devices

--- models/auth_model.py
+++ models/auth_model.py
@@ -115,3 +115,39 @@ def logout_user(db : utils.db_dependency, token : str = Depends(Oauth2.oauth2_ba
 
 
    
+@router.post("/logout-all")
+def logout_user(db : utils.db_dependency, token : str = Depends(Oauth2.oauth2_baerer), refresh_token: Annotated[str | None, Header()] = None):
+    if refresh_token is None:
+        raise HTTPException(status_code=403, detail="unable to find 'Refresh-token' header")
+    
+    try:
+        access_payload = jwt.decode(token, Oauth2.SECRET_KEY, algorithms=Oauth2.ALGORITHM)
+        pass
+    except JWTError:
+        raise HTTPException(status_code=403, detail="token is invalid", headers={"WWW-Authenticate":"Bearer"})
+    
+    try:
+        refresh_payload = jwt.decode(refresh_token, Oauth2.SECRET_KEY, algorithms=Oauth2.ALGORITHM)
+    except JWTError:
+        raise HTTPException(status_code=403, detail="refresh token is invalid", headers={"WWW-Authenticate":"Bearer"})
+    
+    if access_payload.get("type") != "access-token":
+        raise HTTPException(status_code=403, detail="token is invalid", headers={"WWW-Authenticate":"Bearer"})
+
+    if refresh_payload.get("type") != "refresh-token":
+        raise HTTPException(status_code=403, detail="refresh token is invalid", headers={"WWW-Authenticate":"Bearer"})
+
+    if (refresh_db_model := db.query(db_models.RefreshTokens).filter(db_models.RefreshTokens.token == refresh_token).first()) is None:
+        raise HTTPException(status_code=403, detail="refresh token is invalid", headers = {"WWW-Authenticate":"Bearer"})
+
+    
+
+    if (db.query(db_models.AccessTokens).filter(db_models.AccessTokens.id == refresh_db_model.access_token_id).first()).token != token:
+        raise HTTPException(status_code=403, detail="token is invalid", headers={"WWW-Authenticate":"Bearer"})
+    
+    acc_tokens = db.query(db_models.AccessTokens).filter(db_models.AccessTokens.user_id == refresh_db_model.user_id).all()
+    for i in acc_tokens:
+        db.delete(i)
+    db.commit()
+
+    return {"messsage": "You have been logged out from all logins"}
