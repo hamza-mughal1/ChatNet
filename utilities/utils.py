608860b8ec32697e5d76a2350d18a12359a7d7bb
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-12 13:13:20 +0500

    Add 'generate_image_path' func to 'utils' to generate dynamic path
    It will generate dynamic path for images to access based on current host and port

--- utilities/utils.py
+++ utilities/utils.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from models.database_orm import get_db
 from sqlalchemy.inspection import inspect
+from utilities.settings import setting
+HOST = setting.host
+PORT = setting.port
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -17,3 +20,14 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 def orm_to_dict(obj):
     return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
+
+def generate_image_path(filename, func_path):
+    if func_path == "":
+        return "unable to generate the link"
+    if filename == "None":
+        return "None"
+    
+    print(func_path)
+    func_path = str(HOST) + ":" +str(PORT) + func_path.split("{")[0]
+    final_path = func_path + filename
+    return final_path
