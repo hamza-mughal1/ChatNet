df7835d78e20a85ec43a18e063c296b7ee524bca
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-16 15:10:07 +0500

    Fix [5e8e0c9] create dynamic url for images

--- handlers/posts_handler.py
+++ handlers/posts_handler.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Form, UploadFile
+from fastapi import APIRouter, Depends, Form, UploadFile, Request
 from models.posts_model import PostsModel
 from models import schemas
 from typing import List
@@ -11,28 +11,28 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 posts_model = PostsModel()
 
 @router.get("/", response_model=List[schemas.PostOut])
-def all_posts(db : utils.db_dependency):
-    return posts_model.get_all_posts(db)
+def all_posts(db : utils.db_dependency, request: Request):
+    return posts_model.get_all_posts(db, request)
 
 @router.post("/", response_model=schemas.PostOut, status_code=201)
-async def create_post(db: utils.db_dependency, title: Annotated[str, Form()], content: Annotated[str, Form()], image: Annotated[UploadFile, Form()] = None, token_data: dict = Depends(verify_token)):
-    return await posts_model.create_post(db, title, content, image, token_data)
+async def create_post(db: utils.db_dependency, request: Request, title: Annotated[str, Form()], content: Annotated[str, Form()], image: Annotated[UploadFile, Form()] = None, token_data: dict = Depends(verify_token)):
+    return await posts_model.create_post(db, title, content, image, token_data, request)
 
 @router.get("/{id}", response_model=schemas.PostOut)
-def get_post(db : utils.db_dependency, id: int):
-    return posts_model.get_post(db, id)
+def get_post(db : utils.db_dependency, id: int, request: Request):
+    return posts_model.get_post(db, id, request)
 
 @router.delete("/{id}", response_model=schemas.PostOut)
-def delete_post(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
-    return posts_model.delete_post(db, id, token_data)
+def delete_post(db : utils.db_dependency, id: int, request: Request, token_data: dict = Depends(verify_token)):
+    return posts_model.delete_post(db, id, token_data, request)
 
 @router.post("/like/{id}", response_model=schemas.PostOut)
-def like_post_by_post_id(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
-    return posts_model.like_post(db, id, token_data)
+def like_post_by_post_id(db : utils.db_dependency, id: int, request: Request, token_data: dict = Depends(verify_token)):
+    return posts_model.like_post(db, id, token_data, request)
 
 @router.post("/dislike/{id}", response_model=schemas.PostOut)
-def dislike_post_by_post_id(db : utils.db_dependency, id: int, token_data: dict = Depends(verify_token)):
-    return posts_model.dislike_post(db, id, token_data)
+def dislike_post_by_post_id(db : utils.db_dependency, id: int, request: Request, token_data: dict = Depends(verify_token)):
+    return posts_model.dislike_post(db, id, token_data, request)
 
 @router.get("/likes-list/{post_id}", response_model=List[schemas.LikesList])
 def follower_list(db: utils.db_dependency, post_id: int):
