ab8db122e5862cdfc34388f4a411e809d49a850a
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-18 12:36:45 +0500

    Refactor 'app.py' to run in terminal using 'uvicorn'

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ app.include_router(users_handler.router)
 app.include_router(comments_handler.router)
 app.include_router(auth_router)
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", host="0.0.0.0", port=8000)
