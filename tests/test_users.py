80c93c6f37454402c4466a5df41cefbbba61bd21
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-24 16:28:33 +0500

    Add test for 'all_users' endpoint

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,8 +4,8 @@ from app import app
 client = TestClient(app)
 
 def test_all_users():
-    # response = client.get("/users/")
-    # assert response.status_code == 200
+    response = client.get("/users/")
+    assert response.status_code == 200
 
     # demo test 
     assert 200 == 200
