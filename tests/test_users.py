1a03fff181ae929f7ab24e1c90e9ae9a5353c325
hamza_mughal <hamzaxd1123@gmail.com>
2024-09-26 16:57:28 +0500

    Add tests for user resource

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,29 @@
-from fastapi.testclient import TestClient
-from app import app
+from models import schemas
+from tests.testing_database_setup import client
 
-client = TestClient(app)
-
-def test_all_users():
+def test_all_users(client):
     response = client.get("/users/")
     assert response.status_code == 200
+    if response.json() != []:
+        schemas.UserOut(**response.json()[0])
+        
+    name = "hamza"
+    user_name = "hamzatest"
+    email = "hamzatestuser@gmail.com"
+    password = "Hamza@100"
+    client.post("users/", json={"name": name, "user_name": user_name, "email": email,"password": password})
+    response = client.get("/users/")
+    assert response.status_code == 200
+    if response.json() != []:
+        schemas.UserOut(**response.json()[0])
 
-    # demo test 
-    assert 200 == 200
+def test_create_user(client):
+    name = "hamza"
+    user_name = "hamzatest"
+    email = "hamzatestuser@gmail.com"
+    password = "Hamza@100"
+    response = client.post("users/", json={"name": name, "user_name": user_name, "email": email,"password": password})
+    assert response.status_code == 201
+    schemas.UserOut(**response.json())
+    assert response.json().get("user_name") == user_name
+    assert client.post("users/", json={"name": name, "user_name": user_name, "email": email,"password": password}).status_code == 409
