969bdfa40c949ff24f078cab668bf44b3c745a1a
hamza_mughal <hamzaxd1123@gmail.com>
2024-10-04 17:54:37 +0500

    Create 'redis' connection file to manage 'redis'
    To cache data into redis to reduce latency by not calling db for every call

---
+++ models/redis_setup.py
@@ -0,0 +1,10 @@
+import redis
+from utilities.settings import setting
+
+def get_rds():
+    rds = redis.Redis(host=setting.redis_host, port=setting.redis_port, db=setting.redis_db)
+    try:
+        yield rds
+    finally:
+        rds.close()
+        
