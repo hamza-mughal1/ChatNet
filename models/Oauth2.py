122a1536aced6f2959a1e55615833eb2274b5998
hamza_mughal <hamzaxd1123@gmail.com>
2024-10-05 12:54:26 +0500

    Refactor 'access tokens' cache by reducing db request on each hit

--- models/Oauth2.py
+++ models/Oauth2.py
@@ -30,7 +30,20 @@ def create_token(data: dict, refresh=False):
 def verify_token(db :  db_dependency, rds: rds_dependency, token : str = Depends(oauth2_baerer)):
     cache = rds.get(token)
     if cache:
-        pass 
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+            user_id = payload.get("user_id")
+            user_name  = payload.get("user_name")
+
+            if (user_id is None) or (user_name is None):
+                raise HTTPException(status_code=403, detail="token is invalid", headers = {"WWW-Authenticate":"Bearer"})
+        
+        except JWTError:
+            raise HTTPException(status_code=403, detail="token is invalid", headers={"WWW-Authenticate":"Bearer"})
+        
+        payload.update({"token": token})
+        
+        return payload
     elif db.query(AccessTokens).filter(AccessTokens.token == token).first() is None:
         raise HTTPException(status_code=403, detail="token is invalid", headers = {"WWW-Authenticate":"Bearer"})
     try:
