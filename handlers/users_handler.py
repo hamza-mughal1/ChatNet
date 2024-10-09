3a71bb683c9e18fa6502ee366181bd4039a220f8
hamza_mughal <hamzaxd1123@gmail.com>
2024-10-09 17:42:52 +0500

    Add API rate limit in 'users' resource

--- handlers/users_handler.py
+++ handlers/users_handler.py
@@ -10,15 +10,12 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 users_model = UsersModel()
 
+get_users_api_limit = ApiLimitDependency(req_count=ApiLimitMode.TURTLE.value)
 @router.get("/", response_model=List[schemas.UserOut])
-def all_users(db : db_dependency, request: Request, rds: rds_dependency, page: int = 1):
-    if page < 1:
-        page = 1
-    return users_model.get_all_users(db, request, rds=rds, page=page)
-
-@router.post("/", response_model=schemas.UserOut, status_code=201)
-def create_user(user: schemas.CreateUser, db:  db_dependency, request: Request):
-    return users_model.create_user(user, db, request)
+def all_users(db : db_dependency, request: Request, rds: rds_dependency, limit : Annotated[ApiLimitDependency, Depends(get_users_api_limit)], page: int = 1):
+	if page < 1:
+		page = 1
+	return users_model.get_all_users(db, request, rds=rds, page=page)
 
 if OTP_verification["var"]:
 	create_user_api_limit = ApiLimitDependency(req_count=ApiLimitMode.TURTLE.value)
@@ -31,9 +28,10 @@ else:
 		return users_model.create_user_without_otp(user, db, request)
     
     
+get_user_api_limit = ApiLimitDependency(req_count=ApiLimitMode.PANDA.value)
 @router.get("/{id}", response_model=schemas.UserOut)
-def get_user(db :  db_dependency, id: int, request: Request, rds: rds_dependency):
-    return users_model.get_user(db, id, request, rds)
+def get_user(db :  db_dependency, id: int, request: Request, rds: rds_dependency, limit : Annotated[ApiLimitDependency, Depends(get_user_api_limit)]):
+	return users_model.get_user(db, id, request, rds)
 
 @router.put("/", response_model=schemas.UserOut)
 def update_user(db :  db_dependency, user_data: schemas.UpdateUser, request: Request, rds: rds_dependency, token_data: dict = Depends(verify_token)):
